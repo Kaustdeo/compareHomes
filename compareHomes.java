8c30147807b157f0319ed12d603e212ef5bdcd67
Kaustdeo <kaustubhdeo@msn.com>
2016-09-05 11:57:45 -0400

    fixed houseName issue

--- compareHomes.java
+++ compareHomes.java
@@ -1,14 +1,18 @@
 import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class compareHomes {
+    
+    
     
     public static void main(String[] args) {
     
     String fileName = "C:\\Users\\kaust\\introcs\\compareHomes\\houseValues.txt";
     String line = null;
-    String[] houseName = new String[10];
+    String houseName;
     int bidPrice;
     int holdPeriod;
     int exitPrice;
@@ -20,11 +24,11 @@ public class compareHomes {
         
         while((line = bufferedReader.readLine()) != null) {
             String columns[] = line.split(" ");
-            houseName[i] = columns[0];
+            houseName = columns[0];
             bidPrice = Integer.parseInt(columns[1]);
             holdPeriod = Integer.parseInt(columns[2]);
             exitPrice = Integer.parseInt(columns[3]);
-            System.out.println(houseName + "can be bought for " + bidPrice + " and held for " + holdPeriod + " years, to be sold at " + exitPrice + " .");
+            System.out.println(houseName + " can be bought for " + bidPrice + " and held for " + holdPeriod + " years, to be sold at " + exitPrice + " .");
         }
     }
     catch(FileNotFoundException ex) {
@@ -33,5 +37,7 @@ public class compareHomes {
     catch(IOException ex) {
             System.out.println("Error reading file " + fileName + " .");
         }
+    
+    
     }
 }
